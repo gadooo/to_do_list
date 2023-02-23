988cde41d60765da94eac0321f625dc1dffd96d4
gadooo <mogdad19998@gmail.com>
2023-02-23 17:40:15 +0200

    done

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:to_do_list/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  
   List<Task> tasks = [
-    Task(name: "add new task"),
   ];
   void addTask(String newTaskTilte) {
-    if (tasks[0].name == "add new task") {
-      tasks.remove(tasks[0]);
-    }
     tasks.add(Task(name: newTaskTilte));
     notifyListeners();
   }
--- lib/widget/ListItem.dart
+++ lib/widget/ListItem.dart
@@ -5,7 +5,7 @@ class ListItem extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final Function(bool?) checkboxchange;
-   final Function() listTitleDelete;
+  final Function() listTitleDelete;
   // ignore: use_key_in_widget_constructors
   const ListItem(
       {required this.isChecked,
@@ -15,18 +15,18 @@ class ListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(
-          taskTitle,
-          style: TextStyle(
-              decoration: isChecked ? TextDecoration.lineThrough : null),
-        ),
-        trailing: Checkbox(
-          activeColor: Colors.teal[400],
-          value: isChecked,
-          onChanged: checkboxchange,
-        ),
-         onLongPress :listTitleDelete,
-        );
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.teal[400],
+        value: isChecked,
+        onChanged: checkboxchange,
+      ),
+      onLongPress: listTitleDelete,
+    );
   }
 }
 
